ebce9ac153c963d171c70245e1de8d158562ecd0
Maxime Hagenbourger <maximehg@gmail.com>
2019-03-11 19:47:44 +0100

    :sparkles: :recycle: BracketEntry now supports resetting and final four entries

--- src/Components/Builder/BracketEntry.js
+++ src/Components/Builder/BracketEntry.js
@@ -9,21 +9,41 @@ export default class BracketEntry extends Component {
 
     this.chooseTeam1 = this.chooseTeam1.bind(this);
     this.chooseTeam2 = this.chooseTeam2.bind(this);
+    this.resetResults = this.resetResults.bind(this);
+  }
+
+  resetResults() {
+    this.props.result(14,-1)
+    if(this.props.game % 9 <= 2) {
+      this.props.result(8, -1)
+      this.props.result(12, -1)
+    } else if (this.props.game % 9 <= 4) {
+      this.props.result(9, -1)
+      this.props.result(12, -1)
+    } else if (this.props.game % 9 <= 6) {
+      this.props.result(10, -1)
+      this.props.result(13, -1)
+    } else {
+      this.props.result(11, -1)
+      this.props.result(13, -1)
+    }
   }
 
   chooseTeam1() {
-    this.props.result(this.props.team1)
+    if (!this.props.finalFour) this.resetResults()
+    this.props.result(this.props.game-1, this.props.team1-1)
   }
 
   chooseTeam2() {
-    this.props.result(this.props.team2)
+    if (!this.props.finalFour) this.resetResults()
+    this.props.result(this.props.game-1, this.props.team2-1)
   }
 
   render() {
     return(
         <ListGroup variant="flush">
-          <ListGroup.Item onClick={this.chooseTeam1}>{this.props.team1}</ListGroup.Item>
-          <ListGroup.Item onClick={this.chooseTeam2}>{this.props.team2}</ListGroup.Item>
+          <ListGroup.Item onClick={this.chooseTeam1}>{this.props.teams[this.props.team1-1]}</ListGroup.Item>
+          <ListGroup.Item onClick={this.chooseTeam2}>{this.props.teams[this.props.team2-1]}</ListGroup.Item>
         </ListGroup>
     );
   }
