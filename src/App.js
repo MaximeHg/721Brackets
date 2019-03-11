ab64bf76bfec334ececaf3d71398c605da7e78ff
Maxime Hagenbourger <maximehg@gmail.com>
2019-03-11 19:45:18 +0100

    :recycle: Removed regions for an only builder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import BracketNavbar from './Components/Navbar/Navbar';
-import EastRegion from './Components/Builder/EastRegion';
-import WestRegion from './Components/Builder/WestRegion';
-import SouthRegion from './Components/Builder/SouthRegion';
-import MidwestRegion from './Components/Builder/MidwestRegion';
+import BracketBuilder from './Components/Builder/BracketBuilder';
 import Home from './Components/Home/Home';
 import './App.css';
 
@@ -16,10 +13,7 @@ class App extends Component {
           <BracketNavbar />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/east" component={EastRegion}/>
-            <Route exact path="/south" component={SouthRegion}/>
-            <Route exact path="/west" component={WestRegion}/>
-            <Route exact path="/midwest" component={MidwestRegion}/>
+            <Route exact path="/builder" component={BracketBuilder} />
           </Switch>
         </div>
       </BrowserRouter>
