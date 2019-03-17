c730c5a04837e4df3a2fac56811ad1410092d026
Maxime Hagenbourger <maximehg@gmail.com>
2019-03-17 22:37:39 +0100

    Updates

---
+++ precache-manifest.0469ca7e07f6682992929991261a58a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec8ea62779c1cf539822",
+    "url": "/721Brackets/static/js/runtime~main.ae596553.js"
+  },
+  {
+    "revision": "b19b251ea1ce20d27f8f",
+    "url": "/721Brackets/static/js/main.6400660a.chunk.js"
+  },
+  {
+    "revision": "d9c20512a2abdbb431b6",
+    "url": "/721Brackets/static/js/2.4d6b0ac1.chunk.js"
+  },
+  {
+    "revision": "b19b251ea1ce20d27f8f",
+    "url": "/721Brackets/static/css/main.97709e78.chunk.css"
+  },
+  {
+    "revision": "d87893edbf4bc689fdff4ca19afe6896",
+    "url": "/721Brackets/index.html"
+  }
+];
